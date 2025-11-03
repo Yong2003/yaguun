d044d9a51e395511bfca76e186a19c8d6b218ce7
Your Name <95962379+Yong2003@users.noreply.github.com>
2025-11-03 13:25:18 -0700

    added starter code

---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="ChatServer.cs" company="UofU-CS3500">
+// Copyright (c) 2025 UofU-CS3500. All rights reserved.
+// </copyright>
+
+using Networking;
+
+// ReSharper disable once CheckNamespace
+namespace Chatting;
+
+/// <summary>
+///   A simple ChatServer that handles clients separately and replies with a static message.
+/// </summary>
+public abstract class ChatServer
+{
+    /// <summary>
+    ///   The main program.
+    /// </summary>
+    private static void Main( string[] _ )
+    {
+        Server.StartServer( HandleConnect, 11_000 );
+        Console.Read(); // don't stop the program.
+    }
+
+    /// <summary>
+    ///   <pre>
+    ///     When a new connection is established, enter a loop that receives from and
+    ///     replies to a client.
+    ///   </pre>
+    /// </summary>
+    ///
+    private static void HandleConnect( NetworkConnection connection )
+    {
+        // handle all messages until disconnect.
+        try
+        {
+            while ( true )
+            {
+                var message = connection.ReadLine( );
+
+                connection.Send( "thanks!" );
+            }
+        }
+        catch ( Exception )
+        {
+            // do anything necessary to handle a disconnected client in here
+        }
+    }
+}
